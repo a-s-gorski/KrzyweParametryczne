350acbc42ee298b3753c8929fc5564d70a11a971
Łukasz Józef Bondaruk <l.bondaruk@stud.elka.pw.edu.pl>
2020-04-07 21:10:38 +0200

    Upload File

---
+++ Kod  - kamień milowy 1/test_calculator.hpp
@@ -0,0 +1,15 @@
+#ifndef TEST_CALCULATOR_H
+#define TEST_CALCULATOR_H
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+#include "functions.hpp"
+#include "calculator.hpp"
+
+bool test_calculator_calculate_value();
+bool test_calculator_calculate_serie();
+
+void test_calculator();
+#endif
