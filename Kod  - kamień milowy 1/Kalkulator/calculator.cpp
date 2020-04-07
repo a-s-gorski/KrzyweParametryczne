293969096a9696df3c83a2f3533f08a7cc325898
Łukasz Józef Bondaruk <l.bondaruk@stud.elka.pw.edu.pl>
2020-04-07 21:05:07 +0200

    Small add when num of points equal to 1

--- Kod  - kamień milowy 1/Kalkulator/calculator.cpp
+++ Kod  - kamień milowy 1/Kalkulator/calculator.cpp
@@ -8,12 +8,21 @@
 using namespace std;
 
 Calculator::Calculator(){
+    #ifdef _DEBUG
+        cout << "Calculator: Creating Calculator\n";
+    #endif // _DEBUG
 }
 
 Calculator::~Calculator(){
+    #ifdef _DEBUG
+        cout << "Calculator: Destroying Calculator\n";
+    #endif // _DEBUG
 }
 
 pair<double, double> Calculator::calculate_value(vector<Function*> x_function, vector<Function*> y_function, double t){
+    #ifdef _DEBUG
+        cout << "Calculator: Calculate value for: " << t << "\n";
+    #endif // _DEBUG
 
     pair<double, double> point;
 
@@ -34,15 +43,24 @@ pair<double, double> Calculator::calculate_value(vector<Function*> x_function, v
 }
 
 vector<pair<double, double> > Calculator::calculate_serie(vector<Function*> x_function, vector<Function*> y_function, double left, double right, double points_number){
+    #ifdef _DEBUG
+        cout << "Calculator: Calculate " << points_number << " points, from: " << left << " to: " << right << "\n";
+    #endif // _DEBUG
 
     vector< pair <double, double> > points;
 
-    double step = (right-left)/(points_number-1);
+    if(points_number==1){
+        points.push_back(calculate_value(x_function, y_function, left));
+    }else{
+        double step = (right-left)/(points_number-1);
 
-    for(double i =left; i<=right; i+=step){
-        pair<double, double> point = calculate_value(x_function, y_function, i);
-        points.push_back(point);
+        for(double i =left; i<=right; i+=step){
+            pair<double, double> point = calculate_value(x_function, y_function, i);
+            points.push_back(point);
+        }
     }
+
+
 
     sort(points.begin(), points.end());
 
