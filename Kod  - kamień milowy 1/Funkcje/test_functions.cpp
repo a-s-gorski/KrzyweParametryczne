d6ee902b3926326b3b85f933d44d333a62f6b072
Łukasz Józef Bondaruk <l.bondaruk@stud.elka.pw.edu.pl>
2020-04-08 19:06:07 +0200

    Cammel Case!!!

--- Kod  - kamień milowy 1/Funkcje/test_functions.cpp
+++ Kod  - kamień milowy 1/Funkcje/test_functions.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include "functions.hpp"
-#include "test_functions.hpp"
+#include "Functions.hpp"
+#include "TestFunctions.hpp"
 
 using namespace std;
 
-void test_functions_constructor(){
+void testFunctionsConstructor(){
     FCos func_1;
     FCos func_2 = func_1;
     FCos func_3(func_2);
 }
 
-bool test_functions_check_parameters(){
+bool testFunctionsCheckParameters(){
     bool assert = true;
 
     FSin func;
@@ -22,35 +22,35 @@ bool test_functions_check_parameters(){
     //Correct parameters
     vector<double> test_1{1,2,3};
 
-    assert *= (true==func.check_parameters(test_1));
+    assert *= (true==func.checkParameters(test_1));
 
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     //Wrong zero parameter
     vector<double> test_2{1,0,1};
 
-    assert *= (false==func.check_parameters(test_2));
+    assert *= (false==func.checkParameters(test_2));
 
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     //Too less parameters
     vector<double> test_3{1,2};
 
-    assert *= (false==func.check_parameters(test_3));
+    assert *= (false==func.checkParameters(test_3));
 
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     //Too much parameters
     vector<double> test_4{1,2,3,4};
 
-    assert *= (false==func.check_parameters(test_4));
+    assert *= (false==func.checkParameters(test_4));
 
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     return assert;
 }
 
-bool test_functions_calculate_value(){
+bool testFunctionsCalculateValue(){
     bool assert = true;
 
     FCos func_1;
@@ -59,34 +59,34 @@ bool test_functions_calculate_value(){
     cout << "Calculate value started: \n";
 
     double test = 1;
-    assert *= (cos(test)==func_1.calculate_value(test));
+    assert *= (cos(test)==func_1.calculateValue(test));
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     test = 0;
-    assert *= (cos(test)==func_1.calculate_value(test));
+    assert *= (cos(test)==func_1.calculateValue(test));
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     test = 3.14;
-    assert *= (cos(test)==func_1.calculate_value(test));
+    assert *= (cos(test)==func_1.calculateValue(test));
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     test = 1;
-    assert *= (sin(test)==func_2.calculate_value(test));
+    assert *= (sin(test)==func_2.calculateValue(test));
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     test = 0;
-    assert *= (sin(test)==func_2.calculate_value(test));
+    assert *= (sin(test)==func_2.calculateValue(test));
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     test = 3.14;
-    assert *= (sin(test)==func_2.calculate_value(test));
+    assert *= (sin(test)==func_2.calculateValue(test));
     cout << "   assert: " << (assert ? "Correct\n" : "Failed\n");
 
     return assert;
 }
 
-void test_functions(){
+void testFunctions(){
     cout << "Test Functions.\n\n";
-    cout << "Check parameters: " << (test_functions_check_parameters()? "Correct\n\n" : "Failed\n\n");
-    cout << "Calculate value: " << (test_functions_calculate_value()? "Correct\n\n" : "Failed\n\n");
+    cout << "Check parameters: " << (testFunctionsCheckParameters()? "Correct\n\n" : "Failed\n\n");
+    cout << "Calculate value: " << (testFunctionsCalculateValue()? "Correct\n\n" : "Failed\n\n");
 }
