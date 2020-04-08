ab0f106bd7e89993a98819bd96dfbd9626d290cf
Łukasz Józef Bondaruk <l.bondaruk@stud.elka.pw.edu.pl>
2020-04-08 18:53:50 +0200

    Cammel Case

--- Kod  - kamień milowy 1/Kalkulator/Calculator.cpp
+++ Kod  - kamień milowy 1/Kalkulator/Calculator.cpp
@@ -2,8 +2,8 @@
 #include<cmath>
 #include<vector>
 #include<algorithm>
-#include "functions.hpp"
-#include "calculator.hpp"
+#include "Functions.hpp"
+#include "Calculator.hpp"
 
 using namespace std;
 
@@ -19,7 +19,7 @@ Calculator::~Calculator(){
     #endif // _DEBUG
 }
 
-pair<double, double> Calculator::calculate_value(vector<Function*> x_function, vector<Function*> y_function, double t){
+pair<double, double> Calculator::calculateValue(vector<Function*> x_function, vector<Function*> y_function, double t){
     #ifdef _DEBUG
         cout << "Calculator: Calculate value for: " << t << "\n";
     #endif // _DEBUG
@@ -30,11 +30,11 @@ pair<double, double> Calculator::calculate_value(vector<Function*> x_function, v
     double y = 0;
 
     for(int i=0; i<x_function.size(); i++){
-        x += (*(x_function[i])).calculate_value(t);
+        x += (*(x_function[i])).calculateValue(t);
     }
 
     for(int i=0; i<y_function.size(); i++){
-        y += (*(y_function[i])).calculate_value(t);
+        y += (*(y_function[i])).calculateValue(t);
     }
 
     point = make_pair(x,y);
@@ -42,7 +42,7 @@ pair<double, double> Calculator::calculate_value(vector<Function*> x_function, v
     return point;
 }
 
-vector<pair<double, double> > Calculator::calculate_serie(vector<Function*> x_function, vector<Function*> y_function, double left, double right, double points_number){
+vector<pair<double, double> > Calculator::calculateSerie(vector<Function*> x_function, vector<Function*> y_function, double left, double right, double points_number){
     #ifdef _DEBUG
         cout << "Calculator: Calculate " << points_number << " points, from: " << left << " to: " << right << "\n";
     #endif // _DEBUG
@@ -50,12 +50,12 @@ vector<pair<double, double> > Calculator::calculate_serie(vector<Function*> x_fu
     vector< pair <double, double> > points;
 
     if(points_number==1){
-        points.push_back(calculate_value(x_function, y_function, left));
+        points.push_back(calculateValue(x_function, y_function, left));
     }else{
         double step = (right-left)/(points_number-1);
 
         for(double i =left; i<=right; i+=step){
-            pair<double, double> point = calculate_value(x_function, y_function, i);
+            pair<double, double> point = calculateValue(x_function, y_function, i);
             points.push_back(point);
         }
     }
