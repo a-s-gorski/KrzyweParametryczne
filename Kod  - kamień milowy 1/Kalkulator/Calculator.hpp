4126931a31ff1e50f57cc756a36a46a56ea8ab91
Łukasz Józef Bondaruk <l.bondaruk@stud.elka.pw.edu.pl>
2020-04-08 18:54:32 +0200

    Cammel Case

---
+++ Kod  - kamień milowy 1/Kalkulator/Calculator.hpp
@@ -0,0 +1,20 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <iostream>
+#include <vector>
+#include "Functions.hpp"
+
+using namespace std;
+
+class Calculator{
+
+public:
+    Calculator();
+    ~Calculator();
+    pair<double, double> calculateValue(vector<Function*> x_function, vector<Function*> y_function, double t);
+    vector<pair<double, double> > calculateSerie(vector<Function*> x_function, vector<Function*> y_function, double left, double right, double points_number);
+};
+
+
+#endif
