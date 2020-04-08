ee118f8270957863414e8b9c552f247f43427a69
Łukasz Józef Bondaruk <l.bondaruk@stud.elka.pw.edu.pl>
2020-04-08 19:04:04 +0200

    Cammel Case

---
+++ Kod  - kamień milowy 1/Funkcje/TestFunctions.hpp
@@ -0,0 +1,11 @@
+#ifndef TEST_FUNCTIONS_H
+#define TEST_FUNCTIONS_H
+
+#include "functions.hpp"
+
+bool testFunctionsCheckParameters();
+bool testFunctionsCalculateValue();
+
+void testFunctions();
+
+#endif
