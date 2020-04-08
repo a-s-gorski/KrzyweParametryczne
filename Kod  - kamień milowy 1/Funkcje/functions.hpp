76cde785ff651cb02f8667e6c238854b9b159e4f
Łukasz Józef Bondaruk <l.bondaruk@stud.elka.pw.edu.pl>
2020-04-08 18:46:05 +0200

    Correct file uploaded

--- Kod  - kamień milowy 1/Funkcje/functions.hpp
+++ Kod  - kamień milowy 1/Funkcje/functions.hpp
@@ -1,20 +1,51 @@
-#ifndef CALCULATOR_H
-#define CALCULATOR_H
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
 
-#include <iostream>
-#include <vector>
-#include "functions.hpp"
+#include<iostream>
+#include<vector>
 
 using namespace std;
 
-class Calculator{
-
+class Function{
+protected:
+    int parameters_number;
+    vector<double> parameters;
 public:
-    Calculator();
-    ~Calculator();
-    pair<double, double> calculate_value(vector<Function*> x_function, vector<Function*> y_function, double t);
-    vector<pair<double, double> > calculate_serie(vector<Function*> x_function, vector<Function*> y_function, double left, double right, double points_number);
+    virtual ~Function();
+    virtual bool check_parameters(vector<double> parameters)=0;
+    virtual void set_parameters(vector<double> parameters);
+    virtual double calculate_value(double t)=0;
 };
 
+class FCos: public Function{
+    /*
+        Cos(t) = a*cos(b*t)^c
+        parameters_number: 3
+        parameters: a,b,c
+            a!=0
+            b!=0
+            c!=0
+    */
+public:
+    FCos();
+    ~FCos();
+    bool check_parameters(vector<double> parameters);
+    double calculate_value(double t);
+};
 
+class FSin: public Function{
+    /*
+        Sin(t) = a*sin(b*t)^c
+        parameters_number: 3
+        parameters: a,b,c
+            a!=0
+            b!=0
+            c!=0
+    */
+public:
+    FSin();
+    ~FSin();
+    bool check_parameters(vector<double> parameters);
+    double calculate_value(double t);
+};
 #endif
