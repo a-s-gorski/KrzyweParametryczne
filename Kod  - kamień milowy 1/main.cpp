d2fa1dba8c724124e004e5dd6ac226e2e04c9a9c
Łukasz Józef Bondaruk <l.bondaruk@stud.elka.pw.edu.pl>
2020-04-08 20:54:43 +0200

    Not a correct one but have to be for a while

---
+++ Kod  - kamień milowy 1/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "functions.hpp"
+#include "calculator.hpp"
+#include<cmath>
+#include<vector>
+#include"test_functions.hpp"
+#include"test_calculator.hpp"
+
+using namespace std;
+
+int main()
+{
+    test_functions();
+    test_calculator();
+}
