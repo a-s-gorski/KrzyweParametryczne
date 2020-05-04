50c338eb75f030986da5ab6a351620c160266fea
Łukasz Józef Bondaruk <l.bondaruk@stud.elka.pw.edu.pl>
2020-05-04 12:08:08 +0200

    UnitTest TestFunctions->CheckParameters

--- ParametricCurvesUnitTests/testFunctions.cpp
+++ ParametricCurvesUnitTests/testFunctions.cpp
@@ -16,9 +16,50 @@ namespace TestFunctions
 		{
 			FSin func;
 
-			std::vector<double> parameters{ 1,2,3 };
+			std::vector<double> parameters{ 1, 2, 3 };
 
 			bool expected = true;
+
+			bool actual = func.checkParameters(parameters);
+
+			Assert::AreEqual(expected, actual);
+		}
+
+		TEST_METHOD(Wrong_Zero_Parameter)
+		{
+			FSin func;
+
+			std::vector<double> parameters{ 1, 0, 1 };
+
+			bool expected = false;
+
+			bool actual = func.checkParameters(parameters);
+
+			Assert::AreEqual(expected, actual);
+		}
+
+
+		TEST_METHOD(Too_Less_Parameters)
+		{
+			FSin func;
+
+			std::vector<double> parameters{ 1, 2 };
+
+			bool expected = false;
+
+			bool actual = func.checkParameters(parameters);
+
+			Assert::AreEqual(expected, actual);
+		}
+
+
+		TEST_METHOD(Too_Much_Parameters)
+		{
+			FSin func;
+
+			std::vector<double> parameters{ 1, 2, 3, 4 };
+
+			bool expected = false;
 
 			bool actual = func.checkParameters(parameters);
 
