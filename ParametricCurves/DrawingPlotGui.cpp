b87737b1bfc6edc78695d00f55ce3b41525f788f
agorski2 <a.s.gorski@stud.elka.pw.edu.pl>
2020-06-03 18:18:19 +0200

    addind basic exception handling - throwing excep when wrong type assigned or bad_alloc

--- ParametricCurves/DrawingPlotGui.cpp
+++ ParametricCurves/DrawingPlotGui.cpp
@@ -16,26 +16,45 @@ void DrawingPlotGui<X, Y>::drawPlot() {
 	QPicture drawingCanvas = DrawingPlotGui::drawingPanel;
 	QPainter painting_canvas(&drawingCanvas);
 	painting_canvas.setRenderHint(QPainter::Antialiasing);
-	for (int i = 1; i < DrawingPlot::points.size(); i++) {
-		X x1 = DrawingPlotGui::coordinates[i - 1][0];
-		Y y1 = DrawingPlotGui::coordinates[i - 1][1];
-		X x2 = DrawingPlotGui::coordinates[i][0];
-		Y y2 = DrawingPlotGui::coordinates[i][1];
 
-		painting_canvas.drawLine(x1, y1, x2, y2);
-	}
-	painting_canvas.end();
+	try {
+		X x_test = DrawingPlotGui::coordinates[i - 1][0];
+		Y y_test = DrawingPlotGui::coordinates[i - 1][1];
+		X x_test = DrawingPlotGui::coordinates[i][0];
+		Y y_test = DrawingPlotGui::coordinates[i][1];
+
+		if (typeid(x1).name() != typeid(y1).name) {
+			throw std::runtime_error("not an int");
+		}
+
+		if (x_test == NullPtr) 
+		{
+				throw std::bad_alloc;
+		}
+		
+
+
+		for (int i = 1; i < DrawingPlot::points.size(); i++) {
+			X x1 = DrawingPlotGui::coordinates[i - 1][0];
+			Y y1 = DrawingPlotGui::coordinates[i - 1][1];
+			X x2 = DrawingPlotGui::coordinates[i][0];
+			Y y2 = DrawingPlotGui::coordinates[i][1];
+
+			painting_canvas.drawLine(x1, y1, x2, y2);
+		}
+		painting_canvas.end();
 #ifdef _DEBUG
-	cout << "DrawingPlot : drawing plot lines conecting coordinates \n";
+		cout << "DrawingPlot : drawing plot lines conecting coordinates \n";
 #endif // _DEBUG
 
-	for (int i = 0; i < DrawingPlotGui::points.size(); i++) {
-		delete[] DrawingPlotGui::coordinates[i];
-	}
-	delete[] DrawingPlot::coordinates;
+		for (int i = 0; i < DrawingPlotGui::points.size(); i++) {
+			delete[] DrawingPlotGui::coordinates[i];
+		}
+		delete[] DrawingPlot::coordinates;
 #ifdef _DEBUG
-	cout << "DrawingPlot: destructor\n";
-	cout << "DrawingPlot: deleting dynamic 2d array coordinates\n";
+		cout << "DrawingPlot: destructor\n";
+		cout << "DrawingPlot: deleting dynamic 2d array coordinates\n";
 #endif // _DEBUG
 
-}
+		}
+		}
